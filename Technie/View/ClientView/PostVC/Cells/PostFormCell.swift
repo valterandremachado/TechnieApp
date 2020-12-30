b997fbdeb2d9e518a70313392d3217068a74781d
Valter A. Machado <38581929+valterandremachado@users.noreply.github.com>
2020-12-30 22:38:12 +0800

    worked on the PostForm UI but still have a long way to go to the finish line.

--- Technie/View/ClientView/PostVC/Cells/PostFormCell.swift
+++ Technie/View/ClientView/PostVC/Cells/PostFormCell.swift
@@ -7,20 +7,61 @@
 
 import UIKit
 
+// MARK: - PostFormCell
 class PostFormCell: UITableViewCell {
 
+    
+    // MARK: - Properties
+    lazy var customLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = .systemFont(ofSize: 12)
+//        lbl.backgroundColor = .cyan
+        lbl.textAlignment = .right
+        lbl.text = "Max size is 20 MB"
+        return lbl
+    }()
+    
+    lazy var attachFileBtn: UIButton = {
+        var btn = UIButton(type: .system)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+//        btn.backgroundColor = UIColor(displayP3Red: 235/255, green: 51/255, blue: 72/255, alpha: 0.2)
+//        btn.backgroundColor = .brown
+        btn.setTitle("Attach File", for: .normal)
+//        btn.titleLabel?.font = .boldSystemFont(ofSize: 20)
+//        btn.tintColor = .systemPink
+        btn.contentHorizontalAlignment = .left
+//        btn.isHidden = true
+        btn.withWidth(80)
+        //        btn.addTarget(self, action: #selector(loginBtnPressed), for: .touchUpInside)
+        return btn
+    }()
+    
+    lazy var stackView: UIStackView = {
+        var sv = UIStackView(arrangedSubviews: [attachFileBtn, customLabel])
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .horizontal
+        sv.spacing = 0
+        //        sv.alignment = .center
+        sv.distribution = .fillProportionally
+//        sv.addBackground(color: .lightGray)
+        
+        return sv
+    }()
+    
+    // MARK: - Inits
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 //        backgroundColor = .white
         /// Adding tableView right indicator
 //        self.accessoryType = .disclosureIndicator
         /// Changing selection style
-//        self.selectionStyle = .blue
+        self.selectionStyle = .none
         
 //        self.layer.cornerRadius = 15
 //        self.clipsToBounds = true
         backgroundColor = .yellow
-
+//        setupViews()
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -29,6 +70,13 @@ class PostFormCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    // MARK: - Methods
+    func setupViews() {
+        [stackView].forEach {addSubview($0)}
+        stackView.anchor(top: contentView.topAnchor, leading: contentView.safeAreaLayoutGuide.leadingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.safeAreaLayoutGuide.trailingAnchor, padding: UIEdgeInsets(top: 0, left: self.separatorInset.left + 5, bottom: 0, right: self.separatorInset.right + 15))
+    }
+    // MARK: - Selectors
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -36,28 +84,65 @@ class PostFormCell: UITableViewCell {
 
 }
 
+// MARK: - PostFormProjectTypeCell
 class PostFormProjectTypeCell: UITableViewCell {
     static var cellID = "PostFormProjectTypeCell"
-
+    
+    // MARK: - Properties
+    lazy var projectTypeSwitcher: UISegmentedControl = {
+        let items = ["Long Term", "Short Term"]
+        let seg = UISegmentedControl(items: items)
+        seg.translatesAutoresizingMaskIntoConstraints = false
+        seg.selectedSegmentIndex = 0
+        seg.backgroundColor = .brown
+        
+//        seg.frame = CGRect(x: 35, y: 200, width: 250, height: 50)
+//        seg.addTarget(self, action: #selector(segmentAction(_:)), for: .valueChanged)
+//        self.addSubview(seg)
+        return seg
+    }()
+    
+    // MARK: - Inits
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 //        backgroundColor = .white
         /// Adding tableView right indicator
 //        self.accessoryType = .disclosureIndicator
         /// Changing selection style
-//        self.selectionStyle = .blue
-        
-//        self.layer.cornerRadius = 15
-//        self.clipsToBounds = true
-        backgroundColor = .gray
+        self.selectionStyle = .none
+//        backgroundColor = .gray
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
     
+    // MARK: - Methods
+    func setupViews() {
+        [projectTypeSwitcher].forEach {contentView.addSubview($0)}
+//        self.bringSubviewToFront(projectTypeSwitcher)
+        NSLayoutConstraint.activate([
+            projectTypeSwitcher.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            projectTypeSwitcher.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            projectTypeSwitcher.heightAnchor.constraint(equalToConstant: 30),
+//            projectTypeSwitcher.widthAnchor.constraint(equalToConstant: 100),
+            projectTypeSwitcher.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 25),
+            projectTypeSwitcher.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -25),
+        ])
+//        projectTypeSwitcher.anchor(top: contentView.topAnchor, leading: contentView.safeAreaLayoutGuide.leadingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.safeAreaLayoutGuide.trailingAnchor, padding: UIEdgeInsets(top: 0, left: self.separatorInset.left + 5, bottom: 0, right: self.separatorInset.right + 15))
+    }
+    // MARK: - Selectors
+    @objc func segmentAction(_ segmentedControl: UISegmentedControl) {
+            switch (segmentedControl.selectedSegmentIndex) {
+            case 0:
+                break
+            case 1:
+                break
+            default:
+                break
+            }
+        }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -65,20 +150,58 @@ class PostFormProjectTypeCell: UITableViewCell {
 
 }
 
+// MARK: - PostFormBudgetCell
 class PostFormBudgetCell: UITableViewCell {
     static var cellID = "PostFormBudgetCell"
 
+    // MARK: - Properties
+    lazy var customLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = .boldSystemFont(ofSize: 18)
+//        lbl.backgroundColor = .cyan
+        lbl.textAlignment = .right
+        lbl.text = "PHP"
+        return lbl
+    }()
+    
+    lazy var attachFileBtn: UIButton = {
+        var btn = UIButton(type: .system)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+//        btn.backgroundColor = UIColor(displayP3Red: 235/255, green: 51/255, blue: 72/255, alpha: 0.2)
+//        btn.backgroundColor = .brown
+        btn.setTitle("₱200 - ₱800 / hour", for: .normal)
+//        btn.titleLabel?.font = .boldSystemFont(ofSize: 20)
+//        btn.tintColor = .systemPink
+        btn.contentHorizontalAlignment = .left
+//        btn.isHidden = true
+//        btn.withWidth(80)
+        //        btn.addTarget(self, action: #selector(loginBtnPressed), for: .touchUpInside)
+        return btn
+    }()
+    
+    lazy var stackView: UIStackView = {
+        var sv = UIStackView(arrangedSubviews: [attachFileBtn, customLabel])
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .horizontal
+        sv.spacing = 0
+        //        sv.alignment = .center
+        sv.distribution = .fillEqually
+//        sv.addBackground(color: .lightGray)
+        
+        return sv
+    }()
+    
+    // MARK: - Inits
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 //        backgroundColor = .white
         /// Adding tableView right indicator
 //        self.accessoryType = .disclosureIndicator
         /// Changing selection style
-//        self.selectionStyle = .blue
-        backgroundColor = .green
+        self.selectionStyle = .none
+//        backgroundColor = .green
 
-//        self.layer.cornerRadius = 15
-//        self.clipsToBounds = true
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -87,6 +210,14 @@ class PostFormBudgetCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    // MARK: - Methods
+    func setupViews() {
+        [stackView].forEach {contentView.addSubview($0)}
+        stackView.anchor(top: contentView.topAnchor, leading: contentView.safeAreaLayoutGuide.leadingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.safeAreaLayoutGuide.trailingAnchor, padding: UIEdgeInsets(top: 0, left: self.separatorInset.left + 5, bottom: 0, right: self.separatorInset.right + 15))
+    }
+    // MARK: - Selectors
+
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -94,6 +225,7 @@ class PostFormBudgetCell: UITableViewCell {
 
 }
 
+// MARK: - PostFormSkillCell
 class PostFormSkillCell: UITableViewCell {
     static var cellID = "PostFormSkillCell"
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -102,7 +234,7 @@ class PostFormSkillCell: UITableViewCell {
         /// Adding tableView right indicator
 //        self.accessoryType = .disclosureIndicator
         /// Changing selection style
-//        self.selectionStyle = .blue
+        self.selectionStyle = .none
         backgroundColor = .cyan
 
 //        self.layer.cornerRadius = 15
--- Technie/View/ClientView/PostVC/PostFormVC.swift
+++ Technie/View/ClientView/PostVC/PostFormVC.swift
@@ -22,14 +22,14 @@ class PostFormVC: UIViewController {
     var sections = [SectionTitle]()
     // MARK: - Properties
     lazy var tableView: UITableView = {
-        let tv = UITableView(frame: .zero, style: .plain)
+        let tv = UITableView()
         tv.translatesAutoresizingMaskIntoConstraints = false
         tv.backgroundColor = .white
         tv.tableFooterView = UIView()
 
 //        tv.isScrollEnabled = false
         tv.showsVerticalScrollIndicator = false
-//        tv.rowHeight = 40
+        tv.rowHeight = 40
 //        tv.layer.cornerRadius = 18
 //        tv.clipsToBounds = true
 
@@ -44,9 +44,93 @@ class PostFormVC: UIViewController {
         return tv
     }()
     
+    lazy var customLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = .boldSystemFont(ofSize: 18)
+//        lbl.backgroundColor = .cyan
+        lbl.textAlignment = .right
+        lbl.text = "PHP"
+        return lbl
+    }()
+    
+    lazy var budgetPickerBtn: UIButton = {
+        var btn = UIButton(type: .system)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+//        btn.backgroundColor = UIColor(displayP3Red: 235/255, green: 51/255, blue: 72/255, alpha: 0.2)
+//        btn.backgroundColor = .brown
+        btn.setTitle("₱200 - ₱800 / hour", for: .normal)
+//        btn.titleLabel?.font = .boldSystemFont(ofSize: 20)
+//        btn.tintColor = .systemPink
+        btn.contentHorizontalAlignment = .left
+//        btn.isHidden = true
+//        btn.withWidth(80)
+        //        btn.addTarget(self, action: #selector(loginBtnPressed), for: .touchUpInside)
+        return btn
+    }()
+    
+    lazy var stackView: UIStackView = {
+        var sv = UIStackView(arrangedSubviews: [budgetPickerBtn, customLabel])
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .horizontal
+        sv.spacing = 0
+        //        sv.alignment = .center
+        sv.distribution = .fillEqually
+//        sv.addBackground(color: .lightGray)
+        
+        return sv
+    }()
+    
+    var toolBar: UIToolbar = {
+        let toolBar = UIToolbar()
+        toolBar.translatesAutoresizingMaskIntoConstraints = false
+        toolBar.barStyle = .black
+        
+//        let doneBtn = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(onDoneButtonTapped))
+//        let spaceBetweenItems = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+//        let cancelBtn = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(onDoneButtonTapped))
+//        toolBar.items = [cancelBtn, spaceBetweenItems, doneBtn]
+        return toolBar
+    }()
+    
+    lazy var budgetPicker: UIPickerView = {
+        let picker = UIPickerView()
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        picker.delegate = self
+        picker.dataSource = self
+        picker.backgroundColor = UIColor.white
+        picker.setValue(UIColor.black, forKey: "textColor")
+        picker.autoresizingMask = .flexibleWidth
+        picker.contentMode = .center
+        return picker
+    }()
+    
+    lazy var pickerCustomView: UIView = {
+        let cv = UIView()
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.backgroundColor = .white
+        
+        cv.addSubview(budgetPicker)
+//        picker.backgroundColor = .brown
+        budgetPicker.anchor(top: nil, leading: cv.leadingAnchor, bottom: cv.bottomAnchor, trailing: cv.trailingAnchor, padding: UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15), size: CGSize(width: 0, height: 105))
+
+        let doneBtn = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(doneBtnTapped))
+        let spaceBetweenItems = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelBtn = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelBtnTapped))
+        
+        toolBar = UIToolbar.init(frame: CGRect.init(x: 0.0, y: 0, width: UIScreen.main.bounds.size.width, height: 35))
+        toolBar.items = [cancelBtn, spaceBetweenItems, doneBtn]
+        cv.addSubview(toolBar)
+        cv.fadeOut()
+        return cv
+    }()
+    
+    var isPicking = false
+    
+    var customArray: [String] = ["₱200 - ₱300 / hour", "₱300 - ₱400 / hour", "₱400 - ₱500 / hour"]
     let handymanSectionArray = ["Plumbing Installation/Leaking Plumbing", "Drywall Installation", "Fixture Replacement", "Painting for the Interior and Exterior", "Power Washing", "Tile Installation", "Deck/Door/Window Repair", "Carpenter", "Cabinetmaker", "Others"]
 
-    
+    var finalPick = ""
     // MARK: - Inits
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,13 +138,13 @@ class PostFormVC: UIViewController {
         view.backgroundColor = .cyan
         setupViews()
         
-        sections.append(SectionTitle.init(title: "Project Title & Description", description: ["description", "description"]))
-        sections.append(SectionTitle.init(title: "Project Type", description: ["description", "description"]))
-        sections.append(SectionTitle.init(title: "Project Budget", description: ["description", "description"]))
-        sections.append(SectionTitle.init(title: "Skills Required", description: ["description", "description"]))
+        sections.append(SectionTitle.init(title: "Project Title & Description", description: ["0", "1", ""]))
+        sections.append(SectionTitle.init(title: "Project Type", description: ["0"]))
+        sections.append(SectionTitle.init(title: "Project Budget", description: ["0"]))
+        sections.append(SectionTitle.init(title: "Skills Required", description: ["Skill 1", "Skill 2", "Skill 3", "Add Skills"]))
 
     }
-    
+
     // MARK: - Methods
     fileprivate func setupViews() {
         [tableView].forEach {view.addSubview($0)}
@@ -68,6 +152,7 @@ class PostFormVC: UIViewController {
     }
     
     // MARK: - Selectors
+
 
 
 }
@@ -98,17 +183,49 @@ extension PostFormVC: TableViewDataSourceAndDelegate {
         switch indexPath.section {
         case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: tableCellID, for: indexPath) as! PostFormCell
+            cell.backgroundColor = .white
+            
             cell.textLabel?.text = title
+            
+            switch indexPath.row {
+            case 0:
+                let projectTitleTextField = UITextField()
+                projectTitleTextField.placeholder = "Title"
+                projectTitleTextField.backgroundColor = .white
+                
+                cell.addSubview(projectTitleTextField)
+                //                cell.sendSubviewToBack(viewC)
+                projectTitleTextField.anchor(top: cell.topAnchor, leading: cell.textLabel?.leadingAnchor, bottom: cell.bottomAnchor, trailing: cell.textLabel?.trailingAnchor)
+            case 1:
+                let projectDescriptionTextField = UITextField()
+                projectDescriptionTextField.placeholder = "Description"
+                projectDescriptionTextField.backgroundColor = .white
+                                
+                cell.addSubview(projectDescriptionTextField)
+                //                cell.sendSubviewToBack(viewC)
+                projectDescriptionTextField.anchor(top: cell.topAnchor, leading: cell.textLabel?.leadingAnchor, bottom: cell.bottomAnchor, trailing: cell.textLabel?.trailingAnchor)
+            case 2:
+                cell.setupViews()
+            default:
+                break
+            }
+            
             return cell
         case 1:
             let cell = tableView.dequeueReusableCell(withIdentifier: PostFormProjectTypeCell.cellID, for: indexPath) as! PostFormProjectTypeCell
-//            cell.withHeight(100)
+            cell.setupViews()
             return cell
         case 2:
             let cell = tableView.dequeueReusableCell(withIdentifier: PostFormBudgetCell.cellID, for: indexPath) as! PostFormBudgetCell
+            [stackView].forEach {cell.contentView.addSubview($0)}
+            stackView.anchor(top: cell.contentView.topAnchor, leading: cell.contentView.safeAreaLayoutGuide.leadingAnchor, bottom: cell.contentView.bottomAnchor, trailing: cell.contentView.safeAreaLayoutGuide.trailingAnchor, padding: UIEdgeInsets(top: 0, left: cell.separatorInset.left + 5, bottom: 0, right: cell.separatorInset.right + 15))
+//            cell.setupViews()
+            budgetPickerBtn.addTarget(self, action: #selector(budgetPickerBtnTapped), for: .touchUpInside)
             return cell
         case 3:
             let cell = tableView.dequeueReusableCell(withIdentifier: PostFormSkillCell.cellID, for: indexPath) as! PostFormSkillCell
+            cell.textLabel?.text = title
+
             return cell
         default:
             break
@@ -116,17 +233,82 @@ extension PostFormVC: TableViewDataSourceAndDelegate {
 
         return UITableViewCell()
     }
+   
+    fileprivate func setupPickerView() {
+        self.view.addSubview(pickerCustomView)
+        NSLayoutConstraint.activate([
+            pickerCustomView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            pickerCustomView.heightAnchor.constraint(equalToConstant: 140),
+            pickerCustomView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pickerCustomView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+        pickerCustomView.fadeIn()
+
+//        NSLayoutConstraint.activate([
+//            picker.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+////            picker.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+//            picker.heightAnchor.constraint(equalToConstant: 100),
+////            projectTypeSwitcher.widthAnchor.constraint(equalToConstant: 100),
+//            picker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+//            picker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+//        ])
+
+    }
+    
+    @objc func budgetPickerBtnTapped(_ sender: UIButton) {
+        setupPickerView()
+    }
+    
+    @objc func doneBtnTapped() {
+        pickerCustomView.removeFromSuperview()
+        print("123: "+finalPick)
+        if finalPick != "" {
+        budgetPickerBtn.setTitle(finalPick, for: .normal)
+        }
+        pickerCustomView.fadeOut()
+    }
+    
+    @objc func cancelBtnTapped() {
+        pickerCustomView.fadeOut()
+        pickerCustomView.removeFromSuperview()
+//        if finalPick == "" {
+        budgetPicker.selectRow(0, inComponent: 0, animated: true)
+//        } else {
+//            print("finalPick is not empty")
+//        }
+    }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        if section > 0 {
+        if section > 0 {
         return sections[section].title
-//        }
-//        return ""
+        }
+        return ""
     }
 
     
 }
 
+extension PostFormVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return customArray.count
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        print("\(customArray[row])")
+        return customArray[row]
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        print(customArray[row])
+        finalPick = customArray[row]
+    }
+    
+}
 
 
 // MARK: - PostFormPreviews
